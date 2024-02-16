1e89a51edb5f102aa19ef93a21c89f31b4ba7fcd
Dina <dinagiacobbi@gmail.com>
2024-02-16 12:29:20 -0900

    Adding routes and detail pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,31 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
-  // templateUrl: './app.component.html',
+  imports: [
+    HomeComponent,
+    RouterLink,
+    RouterOutlet,
+  ],
   template: `
-  <main>
-    <header class="brand-name">
-      <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
-    </header>
-    <section class="content">
-      <app-home></app-home>
-    </section>
-  </main>
-`,
-  styleUrl: './app.component.css'
+    <main>
+      <a [routerLink]="['/']">
+        <header class="brand-name">
+          <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+        </header>
+      </a>
+      <section class="content">
+        <router-outlet></router-outlet>
+      </section>
+    </main>
+  `,
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'homes';
 }
+
+
--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,23 +1,25 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocation } from '../housinglocation';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
   template: `
     <section class="listing">
       <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
       <h2 class="listing-heading">{{ housingLocation.name }}</h2>
       <p class="listing-location">{{ housingLocation.city}}, {{housingLocation.state }}</p>
+      <a [routerLink]="['/details', housingLocation.id]">Learn More</a>
     </section>
-    `,
+  `,
   styleUrls: ['./housing-location.component.css'],
- 
 })
-
-
 
 export class HousingLocationComponent {
 
@@ -26,6 +28,3 @@ export class HousingLocationComponent {
 }
 
 
-/* Note: The exclamation point is called the non-null assertion operator 
-- it tells the TypeScript compiler that the value of this property 
-won't be null or undefined. */
